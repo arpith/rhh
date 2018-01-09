e194b339125a3d228107eb18f0566a9e348efcc9
Arpith Siromoney <arpith@feedreader.co>
2018-01-09 15:06:41 +0530

    change colors and sort x axis

--- draw.js
+++ draw.js
@@ -3,9 +3,8 @@ import Chart from 'chart.js';
 
 export default function draw(histogram) {
   const canvas = document.getElementById("histogram");
-  const labels = Object.keys(histogram);
+  const labels = Object.keys(histogram).sort();
   const data = labels.map(key => histogram[key]);
-  const backgroundColor = labels.map(() => 'rgba(148, 132, 194, 0.5)');
   const chart = new Chart(canvas, {
     type: 'bar',
     data: {
@@ -13,7 +12,9 @@ export default function draw(histogram) {
       datasets: [{
         label: 'Number of times this graph was generated',
         data,
-        backgroundColor,
+        backgroundColor: 'rgba(255, 225, 207, 0.7)',
+        borderColor: 'rgba(148, 132, 194, 0.9)',
+        borderWidth: 1,
       }],
     },
   });
