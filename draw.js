5e1a130261601304b723351db7018c8af1a5ead7
Arpith Siromoney <arpith@feedreader.co>
2018-01-09 14:58:04 +0530

    switch to chart.js

--- draw.js
+++ draw.js
@@ -1,33 +1,20 @@
 import vis from 'vis';
-
-let mergeCount = 0;
-let stackCount = 0;
-let queueCount = 0;
-let transferCount = 0;
+import Chart from 'chart.js';
 
 export default function draw(histogram) {
-  const container = document.getElementById('histogram');
-  //const items = Object.entries(histogram).map(([x, y]) => {
-    //return {x, y};
-  //});
-  //console.log(items);
-  var items = [
-        {x: '2014-06-11', y: 10},
-        {x: '2014-06-12', y: 25},
-        {x: '2014-06-13', y: 30},
-        {x: '2014-06-14', y: 10},
-        {x: '2014-06-15', y: 15},
-        {x: '2014-06-16', y: 30}
-    ];
-  const options = {
-    style:'bar',
-    barChart: {width:50, align:'center'}, // align: left, center, right
-    drawPoints: false,
-    dataAxis: {
-      icons:true
+  const canvas = document.getElementById("histogram");
+  const labels = Object.keys(histogram);
+  const data = labels.map(key => histogram[key]);
+  const backgroundColor = labels.map(() => 'rgba(148, 132, 194, 0.5)');
+  const chart = new Chart(canvas, {
+    type: 'bar',
+    data: {
+      labels,
+      datasets: [{
+        label: 'Number of times this graph was generated',
+        data,
+        backgroundColor,
+      }],
     },
-    start: '2014-06-10',
-    end: '2014-06-18'
-  };
-  var graph2d = new vis.Graph2d(container, items, options);
+  });
 }
