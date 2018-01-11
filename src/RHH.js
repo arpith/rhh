c025918ca5e188a40a5734df984dd33dd87820d6
Arpith Siromoney <arpith@feedreader.co>
2018-01-11 19:56:55 +0530

    RHH: drop type, simplify isGraphic()
    type was always 'stack', and storage didn't support any other type
    isGraphic is now just popRandom() and transform() - the graph is always
    "graphic". Also, getCycles is now being called in solve.js.

--- src/RHH.js
+++ src/RHH.js
@@ -2,10 +2,9 @@ import Storage from './Storage';
 import Graph from './Graph';
 
 class RHH {
-  constructor(seq, type) {
+  constructor(seq) {
     this.storage = new Storage();
     this.graph = new Graph();
-    this.type = type;
     seq.forEach((v) => {
       this.storage.append(v.degree, v);
     });
@@ -14,7 +13,7 @@ class RHH {
   
   transform(v1) {
     this.graph.addNode(v1);
-    const shiftedNodes = this.storage.shift(v1.degree, this.type);
+    const shiftedNodes = this.storage.shift(v1.degree);
     shiftedNodes.forEach((v) => {
       this.graph.addNode(v);
       this.graph.addEdge(v1, v);
@@ -22,20 +21,10 @@ class RHH {
   }
   
   isGraphic() {
-    let v1 = null;
     while (this.storage.length != 0) {
-      v1 = this.storage.popRandom();
-      if (!v1 || v1.degree == 0) {
-        this.graph.getCycles();
-        return true;
-      } else if (v1.degree >= this.storage.length) {
-        return false;
-      } else if (this.storage.non_zero_count() < v1.degree - 1) {
-        return false;
-      }
+      const v1 = this.storage.popRandom();
       this.transform(v1);
     }
-    this.graph.getCycles();
     return true;
   }
 }
