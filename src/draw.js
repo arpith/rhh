b059b23f472dad0668990f4f37cb6af333d2bbda
Arpith Siromoney <arpith@feedreader.co>
2018-01-11 19:54:56 +0530

    draw: use fractions instead of number of times

--- src/draw.js
+++ src/draw.js
@@ -3,7 +3,8 @@ import Chart from 'chart.js';
 export default function draw(histogram) {
   const canvas = document.getElementById("histogram");
   const labels = Object.keys(histogram).sort();
-  const data = labels.map(key => histogram[key]);
+  const total = Object.values(histogram).reduce((a, v) => (a+v));
+  const data = labels.map(key => histogram[key]/total);
   const chart = new Chart(canvas, {
     type: 'bar',
     options: {
@@ -21,7 +22,7 @@ export default function draw(histogram) {
     data: {
       labels,
       datasets: [{
-        label: 'Number of times this graph was generated',
+        label: 'Fraction of times this graph was generated',
         data,
         backgroundColor: 'rgba(255, 225, 207, 0.7)',
         borderColor: 'rgba(148, 132, 194, 0.9)',
