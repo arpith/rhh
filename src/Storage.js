e8265899382b4ce9a9f556686a098abd132120a4
Arpith Siromoney <arpith@feedreader.co>
2018-01-14 16:54:55 +0530

    Storage: change shift logic
    Assume a vertex, call it v, with degree d<7 is selected in the random process. Then:
    d=1: one of the vertices with degree 9 is selected randomly as its neighbor.
    d=2: two of the vertices with degree 9 are selected randomly as its neighbors.
    d=3: all the vertices with degree 9 are the neighbors of v.
    d=4: all the vertices with degree 9 and one random vertex with degree 8 are the neighbors of v.
    d=5: all the vertices with degree 8 and 9 are the neighbors of v.
    d=6: all the vertices with degree 8 and 9 and one random vertex with degree 7 are the neighbors of v.
    
    Earlier, my understanding of the neighbour selection was:
    
    d=4: among the vertices with degree 9 and 8 (5 vertices) 4 vertices should be selected randomly
    d=5: all vertices with degree 8 and 9 are selected
    d=6: among the vertices with degree 9, 8 and 7 (>= 7 vertices) 6 vertices should be selected randomly
    
    Prof. Bar Noy says:
    "Your implementation doesn't guarantee termination.
    The HH works only if you connect the new vertex with degree d to d vertices with the current
    largest degree breaking ties arbitrarily."

--- src/Storage.js
+++ src/Storage.js
@@ -22,46 +22,78 @@ export default class Storage {
   }
 
   updateHead() {
-    while (!this.store[this.head] || this.store[this.head].length == 0) {
+    while ((!this.store[this.head] || this.store[this.head].length == 0) && (this.head > 0)) {
       this.head -= 1;
     }
   }
 
-  pop(i) {
+  find(i) {
+    // find the list that contains the i'th element
     let j = this.head;
     let count = this.store[j].length;
     while (count <= i) {
-      // find the list that contains i
       j -= 1;
       count += this.store[j].length;
     }
+    return { degree: j, count };
+  }
+
+  pop(i) {
+    const { degree, count } = this.find(i);
     // count - i gives the distance from the end of the last list
     // length - distance from end is distance from start
-    const k = this.store[j].length - (count - i);
-    const vertex = this.store[j].splice(k, 1)[0];
+    const k = this.store[degree].length - (count - i);
+    const vertex = this.store[degree].splice(k, 1)[0];
     this.length -= 1;
     this.updateHead();
     return vertex;
   }
   
-  popRandom(isMax) {
-    if (isMax) {
-      const i = this.randomInt(this.store[this.head].length - 1);
-      return this.pop(i);
-    } else {
-      const i = this.randomInt(this.length - 1);
-      return this.pop(i);
+  popRandom() {
+    const i = this.randomInt(this.length - 1);
+    return this.pop(i);
+  }
+
+  popUntilDegree(degree) {
+    let i = this.head;
+    let ret = [];
+    while (i > degree) {
+      ret = ret.concat(this.store[i].slice());
+      this.store[i] = [];
+      i--;
     }
+    this.length -= ret.length;
+    this.updateHead();
+    return ret;
+  }
+
+  popMultipleRandomFromStack(count, degree) {
+    let i = 0;
+    const ret = [];
+    while (i < count) {
+      const j = this.randomInt(this.store[degree].length - 1);
+      const vertex = this.store[degree].splice(j, 1)[0];
+      ret.push(vertex);
+      i++;
+    }
+    this.length -= ret.length;
+    this.updateHead();
+    return ret;
   }
   
-  shift(count) {
-    const items = [];
-    for (let i = 0; i < count; i++) {
-      const v = this.popRandom(true);
-      v.degree--;
-      items.push(v);
+  shift(number) {
+    if (number === 0) {
+      return [];
     }
-    items.forEach(v => this.append(v.degree, v));
+    const { degree } = this.find(number - 1);
+    let items = this.popUntilDegree(degree);
+    const randomPopCount = number - items.length;
+    const randomPopItems = this.popMultipleRandomFromStack(randomPopCount, degree);
+    items = items.concat(randomPopItems);
+    items.forEach((v) => {
+      v.degree--;
+      this.append(v.degree, v);
+    });
     return items;
   }
 }
